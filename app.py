39b562d7541122ef3d2953c4049f16cee555755a
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-10-01 17:22:44 -0700

    added new route

--- app.py
+++ app.py
@@ -13,11 +13,10 @@ playlists = [
     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
 ]
 
-@app.route('/playlists')
+@app.route('/')
 def playlists_index():
     """Show all playlists."""
     return render_template('playlists_index.html', playlists=playlists)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
